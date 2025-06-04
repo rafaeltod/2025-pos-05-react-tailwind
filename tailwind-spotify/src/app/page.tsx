a6bdffa5ad6913b8593de4472eacac485580884d
rafaeltod <rafael.ricco@escolar.ifrn.edu.br>
2025-06-04 19:33:54 -0300

    Interface Spotify Concluída

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -1,28 +1,102 @@
-import {Home as HomeIcon, Search, Library } from 'lucide-react'
+import { Footer } from "@/components/Footer";
+import { Sidebar } from "@/components/Siderbar";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Play,
+} from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="h-screen flex flex-col">
-    <div className="flex flex-1">
-      <aside className="w-72 bg-zinc-950 p-6">
-        <nav className='space-y-4'>
-          <a href='#' className='flex items-center gap-2 text-xs font-semibold text-zinc-100'>
-            <HomeIcon/>
-            Home
-          </a>
-          <a href='#' className='flex items-center gap-2 text-xs font-semibold text-zinc-100'>
-            <Search/>
-            Search
-          </a>
-          <a href='#' className='flex items-center gap-2 text-xs font-semibold text-zinc-100'>
-            <Library/>
-            Your Library
-          </a>
-        </nav>
-      </aside>
-      <main className="flex-1 p-6">main</main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-3">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum da Taylor Swift" />
+              <strong>1989 (Taylor Version) DELUXE </strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum da Taylor Swift" />
+              <strong>1989 (Taylor Version) DELUXE </strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum da Taylor Swift" />
+              <strong>1989 (Taylor Version) DELUXE </strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum da Taylor Swift" />
+              <strong>1989 (Taylor Version) DELUXE </strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum da Taylor Swift" />
+              <strong>1989 (Taylor Version) DELUXE </strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum da Taylor Swift" />
+              <strong>1989 (Taylor Version) DELUXE </strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+          <h2 className="font-semibold text-2xl mt-10">Made for Rafael Tod</h2>
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" className="w-full" width={120} height={120} alt="Capa do álbum da Taylor Swift" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Kendrick Lamar, Kanye West, Lana del Rey and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" className="w-full" width={120} height={120} alt="Capa do álbum da Taylor Swift" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Kendrick Lamar, Kanye West, Lana del Rey and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" className="w-full" width={120} height={120} alt="Capa do álbum da Taylor Swift" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Kendrick Lamar, Kanye West, Lana del Rey and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" className="w-full" width={120} height={120} alt="Capa do álbum da Taylor Swift" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Kendrick Lamar, Kanye West, Lana del Rey and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" className="w-full" width={120} height={120} alt="Capa do álbum da Taylor Swift" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Kendrick Lamar, Kanye West, Lana del Rey and more</span>
+            </a>
+          </div>
+        </main>
+      </div>
+      <Footer />
     </div>
-    <footer className="bg-zinc-800 border-5 border-zinc-700 p-6">footer</footer>
-  </div>
-  )
+  );
 }
